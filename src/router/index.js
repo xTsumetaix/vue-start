c6f90df8b282a13aa31793c063b54630400df570
berndengels <engels@f50.de>
2021-05-11 15:31:21 +0200

    stuff

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,12 @@ const routes = [
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+/*
+// klassische JS Syntax
+		component: function () {
+			return import('../views/About.vue');
+		}
+*/
 	},
 	{
 		path: '/*',
