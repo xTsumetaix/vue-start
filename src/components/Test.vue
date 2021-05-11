3f7eedd18d2664e76878feaaeaa32f0242238888
berndengels <engels@f50.de>
2021-05-11 12:50:27 +0200

    initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Test",
+    props: ['title'],
+}
+</script>
+
+<style scoped>
+
+</style>
