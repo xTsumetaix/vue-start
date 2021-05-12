bee2a0a705b27c32bcd9e0bd0981bebdd2a26050
berndengels <engels@f50.de>
2021-05-12 16:51:52 +0200

    aufgaben

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
 <style>
 #app {
     color: #2c3e50;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
