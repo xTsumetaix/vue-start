5f2c5e62c8f7c1f3a8e161973ee114eb8855ff8e
berndengels <engels@f50.de>
2021-05-12 14:05:52 +0200

    stuff

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -4,6 +4,11 @@
             <li v-for="item in todos" :key="item.id">
                 <input type="checkbox" v-model="item.done" />
                 <span>{{ item.text }}</span>
+                <b-button
+                    class="btn-sm btn-danger float-right del"
+                >
+                    <font-awesome-icon icon="trash-alt"/>
+                </b-button>
             </li>
         </ul>
         <h3 v-else>Keine Daten vorhanden</h3>
@@ -40,5 +45,37 @@ export default {
 <style scoped>
 .done {
     text-decoration: line-through;
+}
+form {
+    display: flex;
+    width: 100%;
+}
+input[type="checkbox"] {
+    flex: 0.5;
+    margin-left: 1.0rem;
+}
+input[type="text"],
+li span {
+    flex: 10;
+    height: 1.8rem;
+    border: none;
+    margin-left: 1.0rem;
+}
+button {
+    flex: 1;
+}
+li {
+    list-style: none;
+    height: auto;
+    min-height: 2.0rem;
+    line-height: 2.0rem;
+    margin: 5px 10px 0 10px;
+    border: 1px solid #42b983;
+    border-radius: 5px;
+    text-align: left;
+}
+svg[data-icon] {
+    display: inline;
+    margin-right: 5px;
 }
 </style>
