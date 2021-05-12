92d37bb855c12bb912944e835d21e7cd47a5f208
berndengels <engels@f50.de>
2021-05-12 13:20:06 +0200

    stuff

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <ul v-if="todos.length > 0">
-            <li v-for="item in todos" :key="item">
-                {{ item }}
+            <li v-for="item in todos" :key="item.id">
+                <input type="checkbox" v-model="item.done" />
+                <span>{{ item.text }}</span>
             </li>
         </ul>
         <h3 v-else>Keine Daten vorhanden</h3>
@@ -21,13 +22,23 @@ export default {
         this.getTodos()
     },
     methods: {
-        getTodos(){
-            this.todos.push('neues Todo');
+        getTodos() {
+            // eslint-disable-next-line no-undef
+            axios.get("/api/todos")
+                .then(response => {
+                    console.info(response.data.data)
+                    if( !response.data.error ) {
+                        this.todos = response.data.data;
+                    }
+                })
+                .catch(err => console.error(err))
         }
     }
 }
 </script>
 
 <style scoped>
-
+.done {
+    text-decoration: line-through;
+}
 </style>
