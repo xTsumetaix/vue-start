1774a1fa771da2a47293153e0840a58b8281203a
berndengels <engels@f50.de>
2021-05-12 15:02:05 +0200

    stuff

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -3,8 +3,11 @@
         <ul v-if="todos.length > 0">
             <li v-for="item in todos" :key="item.id">
                 <input type="checkbox" v-model="item.done" />
-                <span>{{ item.text }}</span>
+
+                <span :class="{'done': item.done}">{{ item.text }}</span>
+
                 <b-button
+                    @click="remove(item)"
                     class="btn-sm btn-danger float-right del"
                 >
                     <font-awesome-icon icon="trash-alt"/>
@@ -31,13 +34,27 @@ export default {
             // eslint-disable-next-line no-undef
             axios.get("/api/todos")
                 .then(response => {
-                    console.info(response.data.data)
                     if( !response.data.error ) {
                         this.todos = response.data.data;
                     }
                 })
                 .catch(err => console.error(err))
-        }
+        },
+        remove(todo) {
+            if(!confirm("Todo: \"" + todo.text + "\" wirklich löschen?")) {
+                return
+            }
+/*
+            // klassisch js
+            this.todos = this.todos.filter(function (item) {
+                if(item != todo) {
+                    return item
+                }
+            })
+*/
+            // ES6 syntax
+            this.todos = this.todos.filter(item => item !== todo)
+        },
     }
 }
 </script>
