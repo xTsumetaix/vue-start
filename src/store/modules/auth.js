d9bc59f1dd97d2919425185a265927de636d725c
berndengels <engels@f50.de>
2021-05-18 15:32:34 +0200

    stuff

---
+++ src/store/modules/auth.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const auth = {
+	namespaced: true,
+	state: {},
+	getters: {},
+	mutations: {},
+	actions: {},
+}
+export default auth;
---
+++ src/store/modules/todos.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const todos = {
+	namespaced: true,
+	state: {},
+	getters: {},
+	mutations: {},
+	actions: {},
+}
+export default todos;
